e11082fd2263e8a7e31ad8c26b4e91939e8eee25
devk232 <devsadana999@gmail.com>
2020-12-17 01:20:27 +0530

    Added some Algorithms on Graphs

--- Graphs/BFS.cpp
+++ Graphs/BFS.cpp
@@ -2,26 +2,30 @@
 using namespace std;
 
 void BFS(int n,vector<int> v[], int node){
-    bool visited[n];
+    bool visited[n + 1];
     for(int i = 0; i < n; i++)
         visited[i] = false;
     vector <int> a;
-    int distance[n];
+    int distance[n + 1];
+    int pre[n + 1];
     distance[node - 1] = 0;
     queue <int> q;
     q.push(node);
-    visited[node - 1] = true;
+    visited[node] = true;
     a.push_back(node);
     while(!q.empty()){
-        for(int i = 0; i < v[q.front() - 1].size(); i++){
-            if(!visited[v[q.front() - 1][i] - 1]){
-                q.push(v[q.front()- 1][i]);           
-                distance[v[q.front()- 1][i] - 1] = distance[q.front() - 1] + 1;
-                visited[v[q.front()- 1][i]- 1] = true; 
-                a.push_back(v[q.front()- 1][i]);
+        int cur = q.front();
+        q.pop();
+        for(auto i: v[cur]){
+            if(!visited[i]){
+                visited[i] = true;
+                distance[i] = distance[cur] + 1;
+                a.push_back(i);
+                pre[i] = cur;
+                q.push(i);
+
             }
         }
-        q.pop();
     }
     for(int i = 0; i < a.size(); i++){
         cout << a[i] << " ";
@@ -35,11 +39,11 @@ int main(){
     int m;                      // number of vertices
     cin >> m;
     int x, y;
-    vector<int> v[n];          // adjacency list
+    vector<int> v[n + 1];          // adjacency list
     for(int i = 0; i < m; i++){
         cin >> x >> y;
-        v[x - 1].push_back(y);
-        v[y - 1].push_back(x);
+        v[x].push_back(y);
+        v[y].push_back(x);
     }
     int node;                 // starting node 
     cin >> node;
---
+++ Graphs/FloydWarshall.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int dis[n][n], a[n][n];
+// if a[i][j] = 0 then there is no edge between ith and jth vertex, 
+// otherwise it is equal to the length of that edge.
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> a[i][j];
+            if(a[i][j]){
+                dis[i][j] = a[i][j];
+            }
+            else{
+                dis[i][j] = 1e6;
+            }
+        }
+    }
+// dis[i][j] in the kth iteration denotes minimun distance between vertex i and j 
+//such that only first k vertices are used between these two edges.
+    for(int k = 1; k < n + 1; k++){
+        for(int  i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
+            }
+        }
+    }
+    bool ans = false;
+    for(int i  = 0; i < n; i++){
+    	if(dis[i] < 0){
+    		ans = false;
+    		break;
+    	}
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << dis[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
