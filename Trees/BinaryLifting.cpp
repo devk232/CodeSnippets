bc26cc20f1908f89a99b648fc0144c6fe6c3364b
devk232 <devsadana999@gmail.com>
2021-05-18 20:30:23 +0530

    added LCA algorithm

--- Trees/BinaryLifting.cpp
+++ Trees/BinaryLifting.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, log = 20;
+int n, LOG = 20;
 vector<int> v[100001];
 vector<int> depth(100001);
 
@@ -11,7 +11,7 @@ void precompute (vector<int> parent) {
         if(i) depth[i] = depth[parent[i]] + 1;
         v[i][0] = parent[i];
     }
-    for (int j = 1; j < log; j++) {
+    for (int j = 1; j < LOG; j++) {
         for (int i = 0; i < n; i++) {
             v[i][j] = v[v[i][j - 1]][j - 1];  
         }
@@ -21,7 +21,7 @@ void precompute (vector<int> parent) {
 int findAncestor (int node, int k) {
     if (depth[node] < k)
         return -1;
-    for (int i = 0; i < log; i++) {
+    for (int i = 0; i < LOG; i++) {
         if (k & (1 << i)) 
             node = v[node][i]; 
     }
