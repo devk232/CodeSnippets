79dab50f7b3399360bee8ddb3744e878b7a458c2
Dev Krishna Sadana <devsadana999@gmail.com>
2021-09-16 22:58:30 +0530

    Update nCr.cpp

--- Mathematics/nCr.cpp
+++ Mathematics/nCr.cpp
@@ -1,36 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fact[N] , invfact[N];
+ll mod = 1e9 + 7;
+ll N = 1e5 + 1;
+ll fact[100001] , invfact[100001];
 
-int powerM(int a, int b) {
-	int ans = 1;
+ll powerM(ll a, ll b) {
+	ll ans = 1;
 	while (b) {
-		if (b & 1) ans = (ans * a) % mod;
-		b /= 2; a = (a * a) % mod;
-	} return ans;
+	  if (b & 1) ans = (ans * a) % mod;
+	  b /= 2; a = (a * a) % mod;
+	} 
+	return ans;
 }
-int invM(int num) {
+ll invM(ll num) {
 	return powerM(num, mod - 2);
 }
-
 void precompute() {
 	fact[0] = fact[1] = 1;
-	for (int i = 2; i < N; i++) {
-		fact[i] = fact[i - 1] * i;
-		fact[i] %= mod;
-	}
+	for (ll i = 2; i < N; i++) 
+		fact[i] = (fact[i - 1] * i) % mod;
 	invfact[N - 1] = invM(fact[N - 1]);
-	for (int i = N - 2; i >= 0; i--) {
-		invfact[i] = invfact[i + 1] * (i + 1);
-		invfact[i] %= mod;
-	}
+	for (ll i = N - 2; i >= 0; i--) 
+		invfact[i] = (invfact[i + 1] * (i + 1)) % mod;
 }
-
-int nCr(int x, int y) {
+ll nCr(ll x, ll y) {
 	if (y > x) return 0;
-	int num = fact[x];
-	num *= invfact[y]; num %= mod;
-	num *= invfact[x - y];
-	num %= mod; return num;
+	else return (((fact[x]*invfact[y])%mod)*invfact[x - y])%mod;
 }
